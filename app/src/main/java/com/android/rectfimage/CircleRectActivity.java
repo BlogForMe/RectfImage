1634591110113ee4efa15859ac5961c842da50ea
BlogForMe <huaiyiheyuan@gmail.com>
2016-12-19 07:57:34 +0800

    提交

--- app/src/main/java/com/android/rectfimage/CircleRectActivity.java
+++ app/src/main/java/com/android/rectfimage/CircleRectActivity.java
@@ -2,8 +2,14 @@ package com.android.rectfimage;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.android.rectfimage.view.RoundImageViewByXfermode;
@@ -16,14 +22,32 @@ public class CircleRectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_rect);
-        llIv = (LinearLayout) findViewById(R.id.ll_iv);
+//        llIv = (LinearLayout) findViewById(R.id.ll_iv);
 
-        initView();
+//        initView();
+
+        initImg();
+    }
+
+    private void initImg() {
+        ImageView ivIMG = (ImageView) findViewById(R.id.iv_img);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.beaty1, null);
+        Bitmap target = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+
+        RectF rectF = new RectF(0, 0, target.getWidth(), target.getHeight());
+        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Paint paint = new Paint();
+        paint.setShader(shader);
+
+        Canvas canvas = new Canvas(target);
+        canvas.drawRoundRect(rectF, 40, 40, paint);
+        ivIMG.setImageBitmap(target);
+
     }
 
     private void initView() {
         RoundImageViewByXfermode rivbx = new RoundImageViewByXfermode(this);
-        Bitmap bitMap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.aa);
+        Bitmap bitMap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.beaty1);
         rivbx.setImageBitmap(bitMap);
         llIv.addView(rivbx);
     }
--- app/src/main/java/com/android/rectfimage/fragment/Line_Fragment.java
+++ app/src/main/java/com/android/rectfimage/fragment/Line_Fragment.java
@@ -11,7 +11,6 @@ import com.android.rectfimage.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Line_Fragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link Line_Fragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -62,7 +61,7 @@ public class Line_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_line_, container, false);
+        return inflater.inflate(R.layout.fragment_line, container, false);
     }
 
 
